85d24b6d4efb331732a9761f655bab970c7d1e26
cmbaker00 <cmbaker00@gmail.com>
2018-01-22 09:38:24 +1100

    Added python folder to path Changed the plot to only use up to 2015 data

--- python/test.py
+++ python/test.py
@@ -1,7 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-sys.path.insert(0,'/python')
+pth = sys.path
+pth.append(pth[-1] + '/python')
+sys.path = pth
+
+# sys.path.insert(0,'/python')
 from citation_data import *
 
 opts = ['ethics','optim','costben','voi','adaptive','ibm','sdm','ensemble']
@@ -11,7 +15,8 @@ ff = '{0}_fisheries'.format(method)
 fc = '{0}_cons'.format(method)
 fe = '{0}_epi'.format(method)
 
-t, citations = get_citations_by_year('voi_epi')
-plt.plot(t,citations)
+t, citations = get_citations_by_year(ff)
+trange = t <= 2015
+plt.plot(t[trange],citations[trange])
 plt.show()
 
