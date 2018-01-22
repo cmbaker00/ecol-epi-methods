913ef0e1309689c79756c38e431bdba029b1dd78
cmbaker00 <cmbaker00@gmail.com>
2018-01-22 21:12:33 +1100

    Added function for plotting citations by year

--- python/nrm_functions.py
+++ python/nrm_functions.py
@@ -1,4 +1,5 @@
 from citation_data import *
+import matplotlib.pyplot as plt
 
 def get_methods():
     return ['ethics','optim','costben','voi','adaptive','ibm','sdm','ensemble']
@@ -8,4 +9,9 @@ def file_names(method):
     fc = '{0}_cons'.format(method)
     fe = '{0}_epi'.format(method)
     return ff, fc, fe
+
+def plot_citations_year(t, citations, mint = 1950, maxt = 2015):
+    trange = (t <= 2015) & (t > 1950)
+    plt.plot(t[trange], citations[trange])
+    plt.show()
 
