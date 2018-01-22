d39b63f9ade811232244be35e542414ed280ef42
cmbaker00 <cmbaker00@gmail.com>
2018-01-22 20:06:25 +1100

    Return a list of possible methods

--- python/nrm_functions.py
+++ python/nrm_functions.py
@@ -1,7 +1,11 @@
 from citation_data import *
 
+def get_methods():
+    return ['ethics','optim','costben','voi','adaptive','ibm','sdm','ensemble']
+
 def file_names(method):
     ff = '{0}_fisheries'.format(method)
     fc = '{0}_cons'.format(method)
     fe = '{0}_epi'.format(method)
     return ff, fc, fe
+
