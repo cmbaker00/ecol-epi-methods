3bb37b326421aa88ff71773c064b5986f114ca36
cmbaker00 <cmbaker00@gmail.com>
2018-01-23 00:41:06 +1100

    Started plotting functionality for start year

--- python/test.py
+++ python/test.py
@@ -1,20 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-# pth = sys.path
-# pth.append(pth[-1] + '/python')
-# sys.path = pth
-
-# sys.path.insert(0,'/python')
 from citation_data import *
 from nrm_functions import *
 
 opts = get_methods()
 
-method = opts[4]
-# ff, fc, fe = file_names(method)
-#
-# t, citations, papers = get_citations_by_year(ff)
-# plot_citations_year(t, papers)
+start_years = get_all_start_years()
 
-print(get_all_start_years())
+plt.plot(list(start_years['ethics'].values()))
