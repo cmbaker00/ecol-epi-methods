2a71c0b4b22e4a4330e4bbf61a6fcef81c7498d8
cmbaker00 <cmbaker00@gmail.com>
2018-01-23 11:12:31 +1100

    Added call to ordered list of methods

--- python/test.py
+++ python/test.py
@@ -7,6 +7,6 @@ from nrm_functions import *
 opts = get_methods()
 
 start_years = get_all_start_years()
-
+meth_list = ordered_list_methods_by_year()
 plt.plot(list(start_years['ethics'].values()))
 plt.close()
