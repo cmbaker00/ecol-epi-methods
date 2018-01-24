5e8a20442f63e4c6a0580404d60ce816f09b59ce
cmbaker00 <cmbaker00@gmail.com>
2018-01-24 15:13:28 +1100

    Added code for basic scatter plot

--- python/test.py
+++ python/test.py
@@ -8,5 +8,13 @@ opts = get_methods()
 
 start_years = get_all_start_years()
 meth_list = ordered_list_methods_by_year()
-plt.plot(list(start_years['ethics'].values()))
-plt.close()
+cols = {'epi':.4, 'cons': .2, 'fish': .9}
+height = 0
+for method in meth_list:
+    height += 1
+    for fld in start_years['costben']:
+        # continue
+        print(cols[fld])
+        plt.scatter(start_years[method][fld], height, s = 100, c = custom_colours(fld))
+# plt.plot(list(start_years['ethics'].values()))
+# plt.close()
