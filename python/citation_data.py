edc70c42a44afb8a5ab6a2d3a0924cae9e178049
cmbaker00 <cmbaker00@gmail.com>
2018-01-26 17:47:19 +1100

    Finished code to get titles published before a given year

--- python/citation_data.py
+++ python/citation_data.py
@@ -30,22 +30,20 @@ def get_citations_by_year(filename):
 def create_frequency(years, data):
     return [sum(data == t) for t in years]
 
-def get_paper_titles_before(year, method):
-    file = csv.reader(open('../lit/{0}.csv'.format(method), newline=''), delimiter=',')
+def get_paper_titles_before(year, filename):
+    file = csv.reader(open('../lit/{0}.csv'.format(filename), newline=''), delimiter=',')
     titles = []
     flag = False
     for row in file:
         # print(row)
         if row[0] == 'Title':
-            ys = row.index('1900')
-            ye = row.index('2018')
             flag = True
             flag_first = True
         elif flag:
-            y_data = np.array(row[ys:ye]).astype(np.float)
             yr = row[7]
             if yr[0].isalpha():
                 yr = row[8]
+            yr = float(yr)
             if yr <= year:
                 titles.append(row[0])
     return titles
--- python/test.py
+++ python/test.py
@@ -10,11 +10,16 @@ start_years = get_all_start_years()
 meth_list = ordered_list_methods_by_year()
 cols = {'epi':.4, 'cons': .2, 'fish': .9}
 height = 0
-for method in meth_list:
-    height += 1
-    for fld in start_years['costben']:
-        # continue
-        print(cols[fld])
-        plt.scatter(start_years[method][fld], height, s = 100, c = custom_colours(fld))
+# for method in meth_list:
+#     height += 1
+#     for fld in start_years['costben']:
+#         # continue
+#         print(cols[fld])
+#         plt.scatter(start_years[method][fld], height, s = 100, c = custom_colours(fld))
 # plt.plot(list(start_years['ethics'].values()))
 # plt.close()
+fld = 'epi'
+method = 'ensemble'
+
+ff, fc, fe = file_names(method)
+titles = get_paper_titles_before(start_years[method][fld], fe)
